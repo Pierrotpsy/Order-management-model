77fa8a1ab52dc3916d6e07f794da236da015d50c
Pierrotpsy <67154936+Pierrotpsy@users.noreply.github.com>
2022-12-13 20:25:28 +0100

    Update Import ProductVendor.sql

--- Import ProductVendor.sql
+++ Import ProductVendor.sql
@@ -35,31 +35,21 @@ BEGIN
             END IF;
             
             -- extract data from line
-            dbms_output.put_line('begin2');
             productid := REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 1);
-            dbms_output.put_line('begin3');
+            IF productid IS NULL THEN
+                CONTINUE;
+            END IF;
             businessentityid := REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 2);
-            dbms_output.put_line('begin4');
             averageleadtime := REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 3);
-            dbms_output.put_line('begin5');
             standardprice := REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 4);
-            dbms_output.put_line('begin6');
             lastreceiptcost := TO_NUMBER(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 5));
-            dbms_output.put_line('begin7');
             lastreceiptdate := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 6), '(([.]...))', '');
-            dbms_output.put_line('begin8');
             minorderqty := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 7), '( ){1,}', '');
-            dbms_output.put_line('begin9');
             maxorderqty := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 8), '( ){1,}', '');
-            dbms_output.put_line('begin10');
             onorderqty := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 9), '( ){1,}', '');
-            dbms_output.put_line('begin11');
             unitmeasurecode := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 10), '( ){1,}', '');
-            dbms_output.put_line('begin12');
             modifieddate := REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 11), '(([.]...))', '');
-            dbms_output.put_line('begin13');
             
-            --REGEXP_REPLACE(REGEXP_SUBSTR(V_LINE, '[^;]+', 1, 12), '(([.]...))', '');
             
             -- insert extracted data into destination table
             insert into ProductVendor values (
